a4b8a0c5bc9296053c054dfb1e2ea3148871f8c5
Андрей Боев <a.boev.it@gmail.com>
2026-09-07 19:19:36 +0300

    Initial commit

---
+++ 1. Введение/Функция print.py
@@ -0,0 +1,9 @@
+print('Text 1', 'Text 2', 'Text 3', end=' ')
+print('Text 4', 'Text 5', 'Text 6')
+# Результат: Text 1 Text 2 Text 3 Text 4 Text 5 Text 6
+
+print('\'Text 1\'', '\'Text 2\'', '\'Text 3\'', sep='\n')
+# Результат:
+# Text 1
+# Text 2
+# Text 3
