2643947ea9de675606736823c4dab33086de6f9e
Андрей Боев <a.boev.it@gmail.com>
2026-09-21 19:24:06 +0300

    3. Условные конструкции > Координатная плоскость

---
+++ 3. Условные конструкции/Разбор if.py
@@ -0,0 +1,21 @@
+score = 75
+if score >= 90:
+    print("Отлично")
+elif score >= 70:
+    print("Хорошо")
+elif score >= 50:
+    print("Удовлетворительно")
+else:
+    print("Неудовлетворительно")
+
+"""
+score = 75
+if score >= 90:
+    print("Отлично")
+if 70 <= score <= 90:
+    print("Хорошо")
+if 50 <= score <= 70:
+    print("Удовлетворительно")
+if score < 50:
+    print("Неудовлетворительно")
+"""
